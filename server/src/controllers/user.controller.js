9da98b4b27763e87c2074e00277daee2a05320ed
estherlardze <esther.odzao@amalitech.com>
2025-03-03 16:59:48 +0000

    add backend

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -2,5 +2,15 @@ import UserModel from "../models/Users";
 
 
 export const postUser = (req, res) => {
-    const {name, email, age} = req.params;
+}
+
+export const createUser = async(req, res) => {
+    const newUser = new UserModel(req.body);
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+
 }
